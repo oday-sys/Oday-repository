be28f9685efc231fe85e3597c9ab962711fab146
oday-sys <oday.vip394@hotmail.com>
2025-05-03 14:31:42 +0300

    v1.3

--- src/Test.java
+++ src/Test.java
@@ -2,55 +2,28 @@
 public class Test {
 	// my test
 	public static void main(String[] args) {
+	
+		
 		 PhotoManager manager = new PhotoManager();
-		 Photo photo1 = new Photo("pcINroom.jpg",toTagsLinkedList("pc, hardware"));
-		 Photo photo2 = new Photo("gigabyte.jpg",toTagsLinkedList("motherboard, hardware"));
-		 Photo photo3 = new Photo("Msi.jpg",toTagsLinkedList("GPU"));
+		 Photo photo1 = new Photo("hedgehog.jpg",toTagsLinkedList("animal, hedgehog, apple, grass, green"));
 		 manager.addPhoto(photo1);
+		 Photo photo2 = new Photo("bear.jpg",toTagsLinkedList("animal, bear, cab, grass, wind"));
 		 manager.addPhoto(photo2);
+		 Photo photo3 = new Photo("orange-butterfly.jpg",toTagsLinkedList("insect, butterfly, flower, color"));
 		 manager.addPhoto(photo3);
-		 Album album1 = new Album("Parts", "hardware", manager);
-		 Album album2 = new Album("gpus", "GPU", manager);
-		 System.out.println(album1.getPhotos());
-		 System.out.println(album2.getPhotos());
-		 manager.deletePhoto("pcINroom.jpg");
-		 System.out.println(album1.getPhotos());
-		 System.out.println(album2.getPhotos());
-
-
-
-
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-//		 PhotoManager manager = new PhotoManager();
-//		 Photo photo1 = new Photo("hedgehog.jpg",toTagsLinkedList("animal, hedgehog, apple, grass, green"));
-//		 manager.addPhoto(photo1);
-//		 Photo photo2 = new Photo("bear.jpg",toTagsLinkedList("animal, bear, cab, grass, wind"));
-//		 manager.addPhoto(photo2);
-//		 Photo photo3 = new Photo("orange-butterfly.jpg",toTagsLinkedList("insect, butterfly, flower, color"));
-//		 manager.addPhoto(photo3);
-//		 Album album1 = new Album("Album1", "bear", manager);
-//		 Album album2 = new Album("Album2", "animal AND grass", manager);
-//		 System.out.println("Get photo1 path and tags:");
-//		 System.out.println("photo1 path: " + photo1.getPath());
-//		 //You can get the list of tags of photo1 by calling photo1.getTags().
-//		 //You can write a method that prints the list of tags of photo1.
-//		 System.out.println("Get album2 name, condition, and photos:");
-//		 System.out.println("album2 name: " + album2.getName());
-//		 System.out.println("album2 condition: " + album2.getCondition());
-//		 //You can get the list of photos in album2 by calling album2.getPhotos().
-//		 //You can write a method that prints the list of photos in album2.
-//		 System.out.println("Delete the photo ’bear.jpg’:");
-//		 manager.deletePhoto("bear.jpg");
+		 Album album1 = new Album("Album1", "bear", manager);
+		 Album album2 = new Album("Album2", "animal AND grass", manager);
+		 System.out.println("Get photo1 path and tags:");
+		 System.out.println("photo1 path: " + photo1.getPath());
+		 //You can get the list of tags of photo1 by calling photo1.getTags().
+		 //You can write a method that prints the list of tags of photo1.
+		 System.out.println("Get album2 name, condition, and photos:");
+		 System.out.println("album2 name: " + album2.getName());
+		 System.out.println("album2 condition: " + album2.getCondition());
+		 //You can get the list of photos in album2 by calling album2.getPhotos().
+		 //You can write a method that prints the list of photos in album2.
+		 System.out.println("Delete the photo ’bear.jpg’:");
+		 manager.deletePhoto("bear.jpg");
 		 }
 		
 		 private static LinkedList<String> toTagsLinkedList(String tags) {
